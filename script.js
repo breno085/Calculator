804fecd30ed24a94b38522fc66fd007fa9ad1d2e
Breno Acioli <br3noacioli@gmail.com>
2023-07-14 04:08:17 -0300

    Removed some redudant variables

--- script.js
+++ script.js
@@ -55,12 +55,10 @@ function operate(num1, num2, op) {
         }
         op = button.id;
         operando = '';
+
       } else if (typeof num1 === 'number' && typeof num2 === 'number' && button.id === 'equal') {
         document.getElementById("display").innerHTML = Number(operate(num1, num2, op).toFixed(10));
-        num1 = operate(num1, num2, op);
-        num2 = '';
-        op = '';
-        operando = '';
+
       } else if (button.id === 'clear') {
         document.getElementById("display").innerHTML = '';
         num1 = '';
@@ -73,5 +71,7 @@ function operate(num1, num2, op) {
   //Faltando para terminar:
   //Adicionar evento que ao passar o mouse pelos botões ele muda de cor
   //Adicionar outro evento que ao clickar e segurar os botões, eles diminuem um pouco de tamanho
+  //Mudar o texto dos operadores no display
   //Display a snarky error message if the user tries to divide by 0
-  //
+  //Fazer o extra credit: fazer o DEL funcionar e adicionar suporte para o teclado
+  //Pronto!
